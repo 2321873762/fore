3bc571521b629f1679e7ce4dd0d2e06bbdaffd15
sg <131175631@qq.com>
2020-11-23 08:33:33 +0800

    舍根提交

---
+++ box/src/views/舍根/Home.vue
@@ -0,0 +1,369 @@
+<template>
+    <div class="sg_home">       
+        <header>
+            <img src="../../../public/切图/logo.png" alt="" class="header-img">
+            <p class="header-p">设计师</p>
+            <ul class="header-ul">
+                <li>
+                    <i class="iconfont">&#xe600;</i>
+                    <p>我的</p>
+                </li>
+                 <li>
+                    <i class="iconfont">&#xe60d;</i>
+                    <p>导航</p>
+                </li>
+            </ul>
+           
+        </header>
+        <div class="sg_count">
+                <div class="sg_box">
+                        <div class="sg_logo">
+                            <div class="logo-left">
+                                <div class="logo"></div>
+                            </div>
+                            <div class="logo-right">
+                                <span class="name">陈冲</span>
+                                <button>优秀</button>
+                                <p><span>作品：8个</span><span class="jy">经验：5年</span></p>
+                                <p><b>擅长风格：<span>现代/美式/简约/新古典</span></b></p>
+                                <p><b>设计理念：<span>在满足使用功能的前提下达到审美功能的最大化</span></b></p>
+                            </div>
+                        </div>
+                        <ul class="icon-count">
+                            <li>
+                                  <div class="icon-yuan">
+                                      <i class="iconfont">&#xe62e;</i>
+                                  </div>
+                                  <p class="icon-count-p">人气(618)</p>  
+                            </li>
+                             <li>
+                                 <div class="icon-yuan">
+                                      <i class="iconfont icont">&#xe62c;</i>
+                                 </div>
+                                  <p class="icon-count-p">评价(10)</p>  
+                            </li>
+                             <li>
+                                 <div class="icon-yuan">
+                                      <i class="iconfont icont">&#xe625;</i>
+                                 </div>
+                                  <p class="icon-count-p">找我设计</p>  
+                            </li>
+                        </ul>  
+                        <div class="sg_count-text">
+                            <p class="sg_count-text-p"> <span class="sg_count-text-span">个人简介：</span>毕业于中国美术学院，从事家装行业3年，积累了一定的经验。具有良好的审美能力以及独立完成项目方案设计的能力。工作期间曾完成多个室内精装修项目，熟悉室内装修施工工艺及材料的引用。切合实际生活的设计，营造舒适的私人空间。</p>
+                        </div> 
+                        <div class="sg_list">
+                            <p class="sg_list-p"><span class="sg_list-h">&nbsp;&nbsp;&nbsp;&nbsp;设计作品</span><span class="sg_list-span">设计收费50-100/m</span></p>
+                            <ul class="sg_list-ul">
+                                <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                                 <li>
+                                    <img src="../../../public/切图/img1.jpg" alt="" class="sg_list-img">
+                                    <h3 class="sg_list-h">欧美文化的抽象美感</h3>
+                                    <p class="sg_list-p-x">混搭 三室 1100/m</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="sg_wen">
+                            <p class="sg_wen-p">房先生互联网家装有别于传统装修公司与其他装修中介平台，以自营模式整合设计师与项目经理，杜绝项目转包外包，利用互联网将工地进行在线全景直播，通过去中间环节，提供装修套餐+个性化的定制服务</p>
+                            <p class="sg_wen-phone">服务热线：400-9977-658</p>
+                            <ul class="sg_wen_ul">
+                                <li class="active">触屏版</li>
+                                <li>电脑版</li>
+                                <li>关于我们版</li>
+                                <li>联系我们</li>
+                            </ul>
+                            <p class="sg_foooter">@2016sirfang.com 刚房装修就找房先生</p>
+                        </div>
+                </div>
+        </div>
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.sg_home{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    header{
+        width: 100%;
+        height: 50px;
+        display: flex;
+      
+        justify-content: space-between;
+        line-height: 50px;
+      
+        .header-img{
+            width: 120px;
+            height: 44px;
+        }
+        .header-p{
+            flex: 1;
+            font-size: 18px;
+            text-align: center;
+        }
+        .header-ul{
+            display: flex;
+            width: 80px;
+            height: 50px;
+
+            li{
+                padding-top: 8px;
+                padding-right: 10px;
+                display: flex;
+                flex-direction: column;
+                line-height: 20px;
+                text-align: center;
+                font-size: 10px;
+                color: #999999;
+            }
+        }
+
+    }
+    .sg_count{
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        flex: 1;
+        .sg_box{
+            height: 101%;
+            .sg_logo{
+                width: 100%;
+                height: 150px;
+               
+                display: flex;
+                .logo-left{
+                    width: 120px;
+                    height: 100%;
+                    .logo{
+                        width: 80px;
+                        height: 80px;
+                        border-radius: 50%;
+                        background: chartreuse;
+                        margin-top: 10px;
+                        margin-left: 20px;
+                    }
+                }
+                .logo-right{
+                    flex: 1;
+                    padding: 10px 10px;
+                    .name{
+                        font-size: 14px;
+                        color: #000;
+                    }
+                    .jy{
+                        margin-left: 20px;
+                    }
+                    span{
+                        font-size: 10px;
+                         color: #999;
+                    }
+                    button{
+                        border: none;
+                        background: chartreuse;
+                        margin-left: 10px;
+                        border-radius: 10%;
+                        font-size: 10px;
+                        color: #fff;
+                        text-align: center;
+                    }
+                    b{
+                        font-size: 12px;
+                         color: #000;
+                    }
+                }
+            }
+            .icon-count{
+                width: 100%;
+                height: 150px;
+                display: flex;
+                justify-content: center;
+                li{
+                    width: 33%;
+                    margin-left: 25px;
+                    .icon-yuan{
+                        width: 75px;
+                        height: 75px;
+                        border-radius: 50%;
+                        border: 1px solid red;
+                        text-align: center;
+                        line-height: 75px;
+                        .iconfont{
+                            font-size: 40px !important;
+                            color: chartreuse !important;
+                        }
+                       
+                    }
+                     .icon-count-p{
+                            font-size: 12px;
+                            color: red;
+                            margin-left: 15px;
+                            margin-top: 25px;
+                            margin-bottom: 60px;
+                        }
+                }
+            }
+            .sg_count-text{
+                margin:20px 0px;
+                .sg_count-text-p{
+                    font-size:14px;
+                   
+                }
+                .sg_count-text-span{
+                    font-size: 16px;
+                     color: #000;
+                }
+                border-bottom: 2px solid #eee;
+            }
+            .sg_list{
+                width: 100%;
+                .sg_list-p{
+                    width: 100%;
+                    height: 20px;
+                    display:flex;
+                    justify-content: space-between;
+                    line-height: 20px;
+                  
+                     .sg_list-h{
+                         margin-left: 10px;
+                            text-indent: 1em;
+                            font-size:20px;
+                            color: #000;
+                            border-left:2px solid green;
+                        }
+                    .sg_list-span{
+                        
+                        font-size:12px;
+                        color: #999;
+                    }
+
+                }
+               .sg_list-ul{
+                   width: 100%;
+                   height: 100%;
+                   display:flex;
+                   flex-wrap: wrap;
+                   margin-top: 20px;
+                   li{
+                       text-align: center;
+                       width:50%;
+                       margin-bottom: 10px;
+                       .sg_list-img{
+                           width: 180px;
+                           height: 80px;
+                       }
+                       .sg_list-h{
+                           font-size: 14px;
+                           color: #000;
+                       }
+                       .sg_list-p-x{
+                           font-size: 10px;
+                           color: #999;
+                       }
+                   }
+               }
+               
+            }
+            .sg_wen{
+                margin-top: 20px;
+               
+                   width: 100%;
+                   height: 100px;
+                   .sg_wen-p{
+                       font-size: #999;
+                       font-size: 16px;
+                        margin-bottom: 20px;
+                   }
+                   .sg_wen-phone{
+                       width: 100%;
+                       text-align: center;
+                        font-size: 16px;
+                        color: #999;
+                   }
+                   .sg_wen_ul{
+                       width: 100%;
+                       height: 50px;
+                       display: flex;
+                       li{
+                           width: 25%;
+                           height: 50px;
+                           line-height: 50px;
+                           text-align: center;
+                           font-size: 16px;
+                           color: #999;
+                       }
+                       .active{
+                           color: green;
+                       }
+                       
+                   }
+                   .sg_foooter{
+                           font-size: 10px;
+                           color: #999;
+                           width: 100%;
+                           text-align: center;
+                           margin-top: 20px;
+                           margin-bottom: 20px;
+                       }
+               }
+            
+        }
+    }
+    footer{
+        width: 100%;
+        height: 50px;
+        background: pink;
+    }
+}
+</style>
